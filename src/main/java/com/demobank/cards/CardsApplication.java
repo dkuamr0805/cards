cdfdfdb9c601e202d331345d34431bf2f067615c
Dharmendra Kumar <dkumar0805@gmail.com>
2023-12-19 15:48:09 +0530

    config server changes added

--- src/main/java/com/demobank/cards/CardsApplication.java
+++ src/main/java/com/demobank/cards/CardsApplication.java
@@ -2,8 +2,12 @@ package com.demobank.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.demobank.cards.dto.CardsContactInfoDto;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 public class CardsApplication {
 
 	public static void main(String[] args) {
